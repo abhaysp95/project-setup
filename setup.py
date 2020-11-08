755e6a48503ed7a9e4f40bb2cc2f32f5ceaae6c9
coolabhays <abhaysp9955@gmail.com>
2020-11-08 13:05:18 +0530

    add more things

--- setup.py
+++ setup.py
@@ -6,20 +6,29 @@ def read(fname):
 
 setup(
     name='mkproj',
-    version='1.0',
-    packages=find_packages('project'),
+    version='0.0.1',
+    packages=find_packages('src'),
     description='create projects in cli',
     author='Abhay Shanker Pathak',
     author_email='abhaysp9955@gmail.com',
-    platform='linux/unix',
     keywords="project programming language",
     long_description=read('README.md'),
+    url="https://github.com/coolabhays/project-setup",
+    license="MIT",
+    package_dir={'': 'src'},
+    py_modules=["mkproj"],
     install_requires=[
         'Click',
     ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Utilites",
+        "Operating System :: Linux/Unix"
+    ],
     entry_points={
         'console_scripts': [
-            'mkproj=project.app:main',
+            'mkproj=src.app:main',
         ],
     },
 )
